d72d5ab479c2a10ef7d832007c60c5c8b74c639f
Arthur Baratov <greemermon@icloud.com>
2023-09-07 23:58:05 +0300

    Commit-cat

---
+++ js/script.js
@@ -0,0 +1,74 @@
+const nameCats = [
+  "Кошка с колпаком",
+  "Кот с тыквой",
+  "Кошка-балерина",
+  "Кот с усами",
+];
+const imageCats = [
+  "images/1.png",
+  "images/2.png",
+  "images/3.png",
+  "images/4.png",
+];
+
+const infoCats = [
+  {
+    type: "Крутой",
+    color: "Белый с рыжими глазами",
+    vaccine: "Вакцинирована",
+    passport: "Есть ветпаспорт",
+    things: "Приучен к когтеточке",
+    tray: "Знает лоточек",
+  },
+  {
+    type: "Милый",
+    color: "Белый с рыжими глазами",
+    vaccine: "Вакцинирована",
+    passport: "Есть ветпаспорт",
+    things: "Приучен к когтеточке",
+    tray: "Знает лоточек",
+  },
+  {
+    type: "Хороший",
+    color: "Белый с рыжими глазами",
+    vaccine: "Вакцинирована",
+    passport: "Есть ветпаспорт",
+    things: "Приучен к когтеточке",
+    tray: "Знает лоточек",
+  },
+  {
+    type: "Прекрасный",
+    color: "Белый с рыжими глазами",
+    vaccine: "Вакцинирована",
+    passport: "Есть ветпаспорт",
+    things: "Приучен к когтеточке",
+    tray: "Знает лоточек",
+  },
+];
+
+for (let i = 0; i < infoCats.length; i++) {
+  const cat = infoCats[i];
+  const infoList = `
+    <ul>
+      <li>${cat.type}</li>
+      <li>${cat.color}</li>
+      <li>${cat.vaccine}</li>
+      <li>${cat.passport}</li>
+      <li>${cat.things}</li>
+      <li>${cat.tray}</li>
+    </ul>`;
+  const targetElement = document.querySelector(`#name-cat-${i + 1}`);
+  targetElement.insertAdjacentHTML("afterend", infoList);
+}
+
+for (let i = 0; i < nameCats.length; i++) {
+  const dataNameCats = document.createTextNode(nameCats[i]);
+  const textCats = document.getElementById(`name-cat-${i + 1}`);
+  textCats.appendChild(dataNameCats);
+}
+const imagePaths = document.querySelectorAll(".thumb img");
+
+for (let i = 0; i < imagePaths.length; i++) {
+  imagePaths[i].src = imageCats[i];
+  imagePaths[i].alt = nameCats[i];
+}
